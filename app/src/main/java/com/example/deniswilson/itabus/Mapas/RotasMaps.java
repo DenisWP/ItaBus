655c0c7b11d8d8b39d7320dc69361e7b65526fd1
DenisWP <denis.dabliu@gmail.com>
2016-11-02 16:19:56 -0200

    Implementações - API Google Maps [Ajuste no posicionamento do mapa]

--- app/src/main/java/com/example/deniswilson/itabus/Mapas/RotasMaps.java
+++ app/src/main/java/com/example/deniswilson/itabus/Mapas/RotasMaps.java
@@ -2,10 +2,13 @@ package com.example.deniswilson.itabus.Mapas;
 
 
 import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -38,17 +41,29 @@ public class RotasMaps extends SupportMapFragment implements OnMapReadyCallback 
         /*
         * Habilitando o botão de Zoom*/
         mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL); /*Estilo do mapa*/
 
-        LatLng sydney = new LatLng(-33.87365, 151.20689);
+        LatLng sydney = new LatLng(-20.252740, -43.803107); /*Latitude e Longitude*/
 
+        /*
+        * Instanciando um marcador, que ficará posicionado sobre a cidade de Itabirito.
+        * E adicionando o marcador, e posicionando no mapa na posição.
+        * */
         MarkerOptions marker = new MarkerOptions();
         marker.position(sydney);
-        marker.title("Teste");
-
+        marker.title("Itabirito");
         mMap.addMarker(marker);
-
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
-
+        /*Posicionando o mapa mais próximo da cidade de Itabirito.
+        * Ou seja, ao invés de utilizar o mapa do país completo, aparecerá apenas o mapa da
+        * cidade de Itabirito.
+        * */
+        CameraPosition position = new CameraPosition.Builder().target(sydney).zoom(15).build();
+        CameraUpdate update = CameraUpdateFactory.newCameraPosition(position);
+        mMap.moveCamera(update);
     }
+
+
+
 }
