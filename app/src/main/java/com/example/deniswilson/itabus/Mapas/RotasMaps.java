5a1f6433ba0c822227641070185c1fd3913f8d0b
DenisWP <denis.dabliu@gmail.com>
2016-11-03 20:26:29 -0200

    Implementações - Rotas -  [Testando Polyline]

--- app/src/main/java/com/example/deniswilson/itabus/Mapas/RotasMaps.java
+++ app/src/main/java/com/example/deniswilson/itabus/Mapas/RotasMaps.java
@@ -1,8 +1,14 @@
 package com.example.deniswilson.itabus.Mapas;
 
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.widget.Toast;
 
+import com.example.deniswilson.itabus.Administrador.BD;
 import com.example.deniswilson.itabus.Administrador.Rotas;
 import com.example.deniswilson.itabus.Tabs.InfoUsuarios;
 import com.google.android.gms.maps.CameraUpdate;
@@ -22,6 +28,9 @@ public class RotasMaps extends SupportMapFragment implements OnMapReadyCallback 
                             definindo coordenadas, marcar posição no mapa.*/
 
 
+    private BD database;
+    private SQLiteDatabase conexao;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,53 +46,109 @@ public class RotasMaps extends SupportMapFragment implements OnMapReadyCallback 
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
-         /*
-        * Instanciando a classe infoUsuarios, e atribuindo os valores static
-        * */
-
-        InfoUsuarios codRotas = new InfoUsuarios();
-        String rotas = codRotas.rotas;
-
-
-
-
-
         mMap = googleMap;
         // Cidade de Sidney (Teste)
-
         /*
         * Habilitando o botão de Zoom*/
         mMap.getUiSettings().setZoomControlsEnabled(true);
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL); /*Estilo do mapa*/
 
-        LatLng sydney = new LatLng(-20.252740, -43.803107); /*Latitude e Longitude*/
+        /*LatLng sydney = new LatLng(-20.252740, -43.803107); /*Latitude e Longitude*/
 
         /*
         * Instanciando um marcador, que ficará posicionado sobre a cidade de Itabirito.
         * E adicionando o marcador, e posicionando no mapa na posição.
-        * */
+        *
         MarkerOptions marker = new MarkerOptions();
         marker.position(sydney);
         marker.title("Itabirito");
         mMap.addMarker(marker);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
-        /*Posicionando o mapa mais próximo da cidade de Itabirito.
-        * Ou seja, ao invés de utilizar o mapa do país completo, aparecerá apenas o mapa da
-        * cidade de Itabirito.
-        * */
+
         CameraPosition position = new CameraPosition.Builder().target(sydney).zoom(15).build();
         CameraUpdate update = CameraUpdateFactory.newCameraPosition(position);
-        mMap.moveCamera(update);
+        mMap.moveCamera(update);*/
 
-        /*https://www.youtube.com/watch?v=CCZPUeY94MU*/
+        exibirRotas();
+
+
     }
 
 
     public void exibirRotas(){
-        
+        try {
+            /* Instanciando a classe infoUsuarios, e atribuindo os valores static */
+
+            InfoUsuarios codRotas = new InfoUsuarios();
+            String rotas = codRotas.rotas;
+
+            database = new BD(getActivity()); // Criando a referencia do banco.
+            conexao = database.getWritableDatabase();
+            /*
+            * Fazendo o select na tabela rotas para exibir as rotas.
+            * */
+            Cursor cursor = conexao.rawQuery("SELECT "  +BD.COLUNA_IDR+","
+                                                        +BD.COLUNA_CODIGO_ROTAS+","
+                                                        +BD.COLUNA_LATITUDE_INICIAL+","
+                                                        +BD.COLUNA_LATITUDE_FINAL+","
+                                                        +BD.COLUNA_LONGITUDE_INICIAL+","
+                                                        +BD.COLUNA_LONGITUDE_FINAL+
+                                                        " FROM " +BD.TABELA_ROTAS+" WHERE "
+                                                        +BD.COLUNA_CODIGO_ROTAS+" = " +rotas, null);
+
+            if (cursor.moveToFirst()) {
+
+                /*
+                * Tenho que passar para Double, pois minhas declarações foram Strings.
+                *
+                * 1° - Passagando a origem, no caso, todos os onibus se deslocam da garagem , usando o Marker.
+                * 2° - Passando o destino, usando o addPolyline.
+                * */
+
+                LatLng origem = new LatLng(Double.parseDouble(cursor.getString(2)),
+                                           Double.parseDouble(cursor.getString(3)));
+
+                            mMap.addMarker(new MarkerOptions()
+                                            .position(origem)
+                                            .title("Garagem")
+
+                                            /*
+                                            * Se eu quiser colocar umma imagem de ícone:
+                                            * .icon(BitmapDescriptorFactory.fromResource(R.id.drowable...))
+                                            * */
+
+                            );
+
+
+
+                LatLng destino = new LatLng(Double.parseDouble(cursor.getString(4)),
+                                            Double.parseDouble(cursor.getString(5)));
+
+                            mMap.addPolyline(new PolylineOptions().add(origem,destino)
+                                            .width(10)
+                                            .color(Color.BLUE)
+                            );
+
+                /*Posicionando o mapa mais próximo da cidade de Itabirito.
+                * Ou seja, ao invés de utilizar o mapa do país completo, aparecerá apenas o mapa da
+                * cidade de Itabirito.
+                * */
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(origem));
+
+                CameraPosition position = new CameraPosition.Builder().target(origem).zoom(15).build();
+                CameraUpdate update = CameraUpdateFactory.newCameraPosition(position);
+                mMap.moveCamera(update);
+
+            }else {
+                Toast.makeText(getActivity(), "Erro ao carregar rotas.", Toast.LENGTH_SHORT).show();
+            }conexao.close();
+
+        }catch (Exception e){
+            AlertDialog.Builder dlg = new AlertDialog.Builder(getActivity());
+            dlg.setMessage("Erro !" + e.getMessage()); //Retornando o erro que ocorrer.
+            dlg.setNeutralButton("OK", null);
+            dlg.show();
+        }
     }
-
-
 }
