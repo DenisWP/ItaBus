0719e0734b99f3ed87cad4ecdc771ba404ecf6b7
DenisWP <denis.dabliu@gmail.com>
2016-11-18 19:43:44 -0200

    Implementações - [Implementações- Fale Conosco (Ligação)]

--- app/src/main/java/com/example/deniswilson/itabus/Tabs/Inicio.java
+++ app/src/main/java/com/example/deniswilson/itabus/Tabs/Inicio.java
@@ -2,7 +2,9 @@ package com.example.deniswilson.itabus.Tabs;
 
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.deniswilson.itabus.Listar.ListarMunicipal;
 import com.example.deniswilson.itabus.R;
@@ -20,23 +23,23 @@ import com.example.deniswilson.itabus.R;
 
 public class Inicio extends Fragment {
 
+    ImageButton ligue;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.act_tela_inicial, container, false);
 
-        /*
-        *  Criando função OnclickListener, para chamar a tela de lista. Porque estou usando o Fragment
-        *
-        bPesqBairro = (Button) rootView.findViewById(R.id.btagM_pesqbairro);
-        bPesqBairro.setOnClickListener(new View.OnClickListener() {
+
+        ligue = (ImageButton) rootView.findViewById(R.id.imgligue);
+        ligue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent irMunicipal = new Intent(getActivity(), ListarMunicipal.class);
-                startActivity(irMunicipal);
+                Uri uri = Uri.parse("tel: 08000246844");
+                Intent intent = new Intent(Intent.ACTION_CALL,uri);
+                startActivity(intent);
             }
-        });*/
+        });
 
         return rootView;
     }
